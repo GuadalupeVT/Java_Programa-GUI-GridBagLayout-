55ef741eaae7139c9da6e61364322c4023c74eac
Guadalupe Vazquez <guvadlt@gmail.com>
2019-01-31 20:34:51 -0600

    Se agrego el texto Include the following:

--- src/Prueba.java
+++ src/Prueba.java
@@ -17,13 +17,17 @@ public class Prueba extends JFrame{
 		
 		
 		Font fuente = new Font("Arial", 523, 14);
-		JLabel label1 = new JLabel("<html><p>The <b>Clasic Form</b> to includes all visible fields for <br> this list </p></html>");
+		JLabel label1 = new JLabel("<html><p> The <b>Clasic Form</b> to includes all visible fields for <br> this list </p></html>");
 		label1.setFont(fuente);
-		metodoMagico(label1, 0, 2, 50,1);
+		metodoMagico(label1, 2, 2, 50,1);
 		
-		JLabel label2 = new JLabel("<html><br>Form options</html>");
+		JLabel label2 = new JLabel("<html><br> Form options</html>");
 		label2.setFont(new Font("Arial", 523, 18));
-		metodoMagico(label2,0,5,50,1);
+		metodoMagico(label2,2,5,50,1);
+		
+		JLabel label3 = new JLabel("Include the following: ");
+		label3.setFont(fuente);
+		metodoMagico(label3,2,10,50,1);
 		
 		pack();
 }
